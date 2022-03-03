3ec406ae34b3bde14368b939925e35726a709935
Pradeep18s <pradeedsriram75@gmail.com>
2022-03-03 12:06:10 +0530

    add

--- src/test/java/org/maven/WebT.java
+++ src/test/java/org/maven/WebT.java
@@ -11,26 +11,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebT {
 	
+	public void add() {
+
+		System.out.println("add");
+		
+	}
+	
 	public static void main(String[] args) {
-		
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		
-		driver.get("https://www.w3schools.com/html/html_tables.asp");
-		WebElement tble = driver.findElement(By.xpath("//*[@id=\"customers\"]"));
-		String text = tble.getText();
-//		System.out.println(text);
-		
-		List<WebElement> elements = tble.findElements(By.tagName("td"));
-		for (int i = 0; i < elements.size(); i++) {
-			
-			WebElement ele = elements.get(i);
-			String text2 = ele.getText();
-			System.out.println(text2);
+		 WebT ob=new WebT();
+		 ob.add();
+	
 			
 		}
 		
 		
 		
 	}
-}
+
